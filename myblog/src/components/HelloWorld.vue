921d49848534973d816eaf9b58b6f1fbfefadd2d
suifeng123 <wswen1220@163.com>
2018-04-21 17:08:34 +0800

    提交一个前端到后台的一个笼统的架构

---
+++ myblog/src/components/HelloWorld.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="hello">
+    <el-form ref="form" :model="form">
+       <el-form-item label="id号">
+            <el-input v-model="form.id"></el-input>
+       </el-form-item>
+       <el-form-item  label="头衔">
+            <el-input v-model="form.title"></el-input>
+       </el-form-item>
+       <el-form-item label="内容">
+            <el-input type="textarea" rows="5" cols="5" v-model="form.content"></el-input>
+       </el-form-item>
+       <el-form-item>
+          <el-button type="primary" @click="onSubmit">立即创建</el-button>
+          <el-button>取消</el-button>
+       </el-form-item>
+
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      form:{
+        id:"",
+        content:"",
+        title:""
+      }
+    }
+  },
+  methods:{
+      onSubmit(){
+        this.axios.post('/api/adduser',{
+          "uid": this.form.id,
+          "content": this.form.content,
+          "title": this.form.title
+        }).then((res) => {
+          console.log(res);
+
+        })
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  position: absolute;
+  left:25%;
+
+  width: 400px;
+  height: 400px;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
