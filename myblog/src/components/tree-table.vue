1f54cdfe1eb5e0f8010da345b6c275f4b99c4316
suifeng123 <wswen1220@163.com>
2018-07-26 17:10:56 +0800

    提交一个tree-table的提交

---
+++ myblog/src/components/tree-table.vue
@@ -0,0 +1,15 @@
+<!-- 进行一种树形表格的书写  -->
+/**
+ * 首先进行表单头的
+ */
+<template>
+  <div class="tree-table">
+    <div class="tree-head">
+       <table>
+          <tr>
+             <td class="td1">职位名称</td>
+          </tr>
+       </table>
+    </div>
+  </div>
+</template>
