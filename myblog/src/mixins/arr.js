aaeb60037f8f83aa7edfe2a88ad839ccfc18487d
suifeng123 <wswen1220@163.com>
2018-08-07 18:39:29 +0800

    提交一些固定的object的方法

--- myblog/src/mixins/arr.js
+++ myblog/src/mixins/arr.js
@@ -112,3 +112,31 @@ function processContent(options){
   let options = Object.assign({},)
 }
 
+
+Object.defineProperty(Object.prototype,'__proto__',{
+	get(){
+		let _thisObj = Object(this);
+		return Object.getPrototypeOf(_thisObj);
+	},
+	set(proto){
+		if(this === undefined || this === null){
+			throw new TypeError();
+		}
+		if(!isObject(this)){
+			return undefined;
+		}
+		if(!isObject(proto)){
+			return undefined;
+		}
+		let status = Reflect.setPrototypeOf(this,proto);
+		if(!status){
+			throw new TypeError();
+		}
+	}
+});
+
+function isObject(value){
+	return Object(value) === value;
+}
+
+
