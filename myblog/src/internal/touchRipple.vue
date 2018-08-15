4f2148a8c38d4912fd3f1bc8069731354c3ab9c2
suifeng123 <wswen1220@163.com>
2018-08-15 16:05:09 +0800

    tijiao

---
+++ myblog/src/internal/touchRipple.vue
@@ -0,0 +1,7 @@
+<template>
+	<div @mousedown="handleMouseDown" @mouseup="end()"
+	     @mouseleave="end()" @touchstart="handleTouchStart()"
+	     @touchend="end()" @touchcancel="end()">
+	
+	</div>
+</template>
