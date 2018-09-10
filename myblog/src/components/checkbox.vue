ca1a6045bcf0a88f917fe6404b00e2504d46c2e8
suifeng123 <wswen1220@163.com>
2018-09-10 16:27:36 +0800

    提交部分属性

--- myblog/src/components/checkbox.vue
+++ myblog/src/components/checkbox.vue
@@ -77,9 +77,123 @@ export default {
 	computed: {
 		model:{
 			get(){
-				
+			   return this.isGroup ? this.store : this.value !== undefined
+			      ? this.value : this.selfModel	
+			},
+			set(val){
+				if(this.isGroup){
+					this.isLimitExceeded = false;
+					(this._checkboxGroup.min !== undefined &&
+						val.length < this._checkboxGroup.min &&
+						(this.isLimitExceeded = true));
+
+					(this._checkboxGroup.max !== undefined &&
+						val.length > this._checkboxGroup.max &&
+						(this.isLimitExceeded = true));
+
+					this.isLimitExceeded === false &&
+					this.dispatch('ElCheckboxGroup','input',[val]);
+				}else{
+					this.$emit('input',value);
+					this.selfModel = val;
+				}
 			}
+		},
+
+		isChecked(){
+			if({}.toString.call(this.model) === '[object Boolean]'){
+			      return this.model	
+			}else if(Array.isArray(this.model)){
+				return this.model.indexOf(this.label) > -1;
+			}else if(this.model !== null && this.model !== undefined){
+				return this.model === this.trueLabel;
+			}
+		},
+
+		isGroup(){
+			let parent = this.$parent;
+			while(parent){
+				if(parent.$options.componentName !== 'ElCheckboxGroup'){
+					parent = parent.$parent;
+				}else {
+					this._checkboxGroup = parent;
+					return true
+				}
+			}
+			return false;
+		},
+		store(){
+			return this._checkboxGroup ? this._checkboxGroup.value : this.value;
+		},
+
+		isDisabled(){
+			return this.isGroup ? this._checkboxGroup.disabled || this.disabled || (this.elForm || {}).disabled : this.disabled || (this.elForm || {}).disabled;
+		},
+
+		_elFormItemSize(){
+		    return (this.elFormItem || {})._elFormItemSize;	
+		},
+
+		checkboxSize(){
+			const temCheckboxSize = this.size || this._elFormItemSize || (this.$ELEMENT || {}).size;
+			return this.isGroup ? this_checkboxGroup.checkboxGroupSize || temCheckboxSize : temCheckboxSize;
 		}
-	}
+    },
+    props:{
+    	value:{},
+    	label:{},
+    	indeterminate: Boolean,
+    	disabled: Boolean,
+    	checked: Boolean,
+    	name: String,
+    	trueLabel:[String,Number],
+    	falseLabel: [String,Number],
+    	id: String,
+    	controls: String,
+    	border: Boolean,
+    	size: String
+    },
+
+    methods:{
+    	addToStore(){
+    		if(Array.isArray(this.model) &&
+    			this.model.indexOf(this.label) === -1){
+    			this.model.push(this.label);
+    		}else{
+    			this.model = this.trueLabel || true;
+    		}
+    	},
+    	handleChange(ev){
+    		if(this.isLimitExceeded) return;
+    		let value;
+    		if(ev.target,checked){
+    			value = this.trueLabel === undefined ? true : this.trueLabel;
+    		}else {
+    			value = this.falseLabel === undefined ? false : this.falseLabel;
+    		}
+
+    		this.$emit('change',value,ev);
+
+    		this.nextTick(() => {
+    			if(this.isGroup){
+    				this.dispatch('ElCheckboxGroup','change',[this._checkboxGroup.value])
+    			}
+    		});
+    	}
+    },
+
+    created(){
+    	this.checked && this.addToStore();
+    },
+    mounted(){
+    	if(this.indeterminate){
+    		this.$el.setAttribute('aria-controls',this.controls);
+    	}
+    },
+    watch:{
+    	value(value){
+    		this.dispatch('ElFormItem','el.form.change',value);
+    	}
+    }
 }
 </script>
