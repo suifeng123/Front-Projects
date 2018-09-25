6f5a9041372a3a06bd376c648d54d26411bb42aa
suifeng123 <wswen1220@163.com>
2018-09-25 15:13:51 +0800

    提交删除部分代码

--- myblog/src/utils/dom.js
+++ myblog/src/utils/dom.js
@@ -66,6 +66,41 @@ export const once = function(el,element,fn){
 export function hasClass(el,cls){
 	if(!el || !cls) return false;
 	if(cls.indexOf(' ') !== -1) throw new Error('className should not contain space.');
+	if(el.classList) {
+		return el.classList.contain(cls);
+	}else{
+		return (' '+el.className + ' ').indexOf(' '+cls + ' ') > -1;
+	}
+};
+
+
+export default addClass(el,cls){
+	if(!el) return;
+	var curClass = el.className;
+	var classes = (cls || '').split(' ');
+
+	for(var i =  0 , j = classes.length; i < j ; i++){
+		var clsName = classes[i];
+		if(!clsName) continue;
+
+		if(el.classList){
+			el.classList.add(clsName);
+		}else if(!hasClass(el,clsName)){
+			curClass += ' '+ clsName;
+		}
+	}
+
+	if(!el.classList){
+		el.className = curClass;
+	}
+}
+
+export function removeClass(el,cls){
+	if(!el || !cls) return;
+	var classes = cls.split(' ');
+	var curClass = ' ' + el.className + ' ';
+
+	for(var i = 0 , j = classes.length)
 }   
 
 	
