89ef11279e2a1c8dbf299526dd47c67e2e174b39
suifeng123 <wswen1220@163.com>
2018-10-11 13:17:43 +0800

    提交代码

---
+++ myblog/src/components/card.vue
@@ -0,0 +1,32 @@
+<template>
+<!-- 进行展示一个card -->
+  <div class="my-card" :class="shadow ? 'is-' + shadow + '-shadow' : 'is-always-shadow'">
+      <div class="my-card__header" v-if="$slots.header || header">
+          <slot name="header">{{header}}</slot>
+      </div>
+      <div class="my-card__body" :style="bodyStyle">
+         <slot></slot>
+      </div>
+  </div>
+</template>
+<style scoped>
+ .my-card {
+ 	border-radius: 4px;
+ 	border: 1px solid #ebeef5;
+ 	background-color: #ffff;
+ 	overflow: hidden;
+ 	color: #303133;
+ 	transition: .3s;
+ }
+</style>
+<script>
+  export default {
+  	 props:{
+  	 	header:{},
+  	 	bodyStyle:{},
+  	 	shadow:{
+  	 		type: String
+  	 	}
+  	 }
+  }
+</script>
