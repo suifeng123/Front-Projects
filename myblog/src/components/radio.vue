1022b15921f0fdad368ac7b51758d368a0c73b17
suifeng123 <wswen1220@163.com>
2018-10-17 09:14:06 +0800

    提交性能监控的部分代码

---
+++ myblog/src/components/radio.vue
@@ -0,0 +1,17 @@
+//进行编写一个radio的组件  仿照element-ui  学习element-ui 源码
+<template>
+    <label class="el-radio-button"
+         :class="[
+	      size ? 'el-radio-button--' + size : '',
+	      { 'is-active': value === label },
+	      { 'is-disabled': isDisabled },
+	      { 'is-focus': focus }
+	 ]"
+	  role="radio"
+	  :aria-checked="value === label"
+	  :aria-disabled="isDisabled"
+	  :tabindex="tabIndex"
+	  @keydown.space.stop.prevent="value=isDisabled ? value : label"
+	  >
+     </label>
+</templte>
