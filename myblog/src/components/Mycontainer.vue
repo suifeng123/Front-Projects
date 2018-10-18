43306d2f6dd3de91a58dcd8e94d73ed22df73329
suifeng123 <wswen1220@163.com>
2018-10-18 09:54:19 +0800

    提交一个elementui的组件

---
+++ myblog/src/components/Mycontainer.vue
@@ -0,0 +1,38 @@
+<template>
+<!-- 进行书写一个容器 -->
+<section class="my-container" :class="{ 'is-vertical': isVertical }">
+    <slot></slot>
+</section>
+</template>
+
+<script>
+export default {
+	name: 'ElContainer',
+	componentName: 'ElContainer',
+
+	props:{
+		direction: String,
+	},
+
+	computed: {
+		isVertical(){
+			if(this.direction === 'vertical'){
+				return true;
+			}else if(this.direction === 'horizontal'){
+				return false;
+			}
+
+			return this.$slots && this.$slots.default ?
+			   this.$slots.default.some(vnode => {
+			   	   const tag = vnode.componentOptions && vnode.componentOptions.tag;
+			   	   return tag === 'el-header' || tag === 'el-footer';
+			   })
+			   : false
+		}
+	},
+
+	mounted(){
+		//进行挂载后进行的操作
+	}
+}
+</script>
